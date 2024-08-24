fa9539c093e1dcb19a78932a46ab26f0fd4bf953
fejiang <fejiang@nvidia.comm>
2024-08-24 19:38:19 +0800

    check if cudaMemcpy is successful

--- src/main/cpp/src/partitioned_hash_join.cuh
+++ src/main/cpp/src/partitioned_hash_join.cuh
@@ -69,6 +69,7 @@ public:
 //          release_mem(COL(r,0));
 //          release_mem(COL(s,0));
 //  #endif
+
         allocate_mem(&r_key_partitions_temp, true, buckets_num_max_R * bucket_size * sizeof(key_t));
         allocate_mem(&s_key_partitions_temp, true, buckets_num_max_S * bucket_size * sizeof(key_t));
 
@@ -111,15 +112,136 @@ public:
     }
 
     void join(){
+         partition();
+         hash_join();
+    }
+
+    ~PartitionHashJoin() {}
+public:
+    float partition_time {0};
+    float join_time {0};
+    float mat_time {0};
+
+private:
+
+    template<typename KeyT, typename val_t>
+    void partition(KeyT* keys, KeyT* keys_out,
+                       val_t* vals, val_t* vals_out, int n, int buckets_num,
+                       uint64_t* heads[2], uint32_t* cnts[2],
+                       uint32_t* chains[2], uint32_t* buckets_used[2]) {
+        constexpr int NT = (sizeof(KeyT) == 4 ? 1024 : 512);
+        constexpr int VT = 4;
+
+        // shuffle region + histogram region + extra meta info
+        const size_t p1_sm_bytes = (NT*VT) * max(sizeof(KeyT), sizeof(val_t)) + (4*(1 << log_parts1)) * sizeof(int32_t);
+        const size_t p2_sm_bytes = (NT*VT) * max(sizeof(KeyT), sizeof(val_t)) + (4*(1 << log_parts2)) * sizeof(int32_t);
+
+        const int sm_counts = 70; // need to change later.
+
+      // Initialize Metadata:
+        // init_metadata_double: A CUDA kernel that initializes metadata for buckets, such as heads, chains, and counts.
+        init_metadata_double<<<sm_counts, NT, 0>>> (
+            heads[0], buckets_used[0], chains[0], cnts[0],
+            1 << log_parts1, buckets_num,
+            heads[1], buckets_used[1], chains[1], cnts[1],
+            1 << (log_parts1 + log_parts2), buckets_num,
+            bucket_size
+        );
+
+        // partition_pass_one: A CUDA kernel that performs the first pass of partitioning. It distributes keys and
+        // values into initial buckets based on log_parts1.
+        partition_pass_one<NT, VT><<<sm_counts, NT, p1_sm_bytes>>>(
+                                                    keys,
+                                                    vals,
+                                                    heads[0],
+                                                    buckets_used[0],
+                                                    chains[0],
+                                                    cnts[0],
+                                                    keys_out,
+                                                    vals_out,
+                                                    n,
+                                                    log_parts1,
+                                                    first_bit + log_parts2,
+                                                    log2_bucket_size);
+        // Compute Bucket Information:
+        // compute_bucket_info: A CUDA kernel that processes chain information to prepare for the second pass.
+        compute_bucket_info <<<sm_counts, NT>>> (chains[0], cnts[0], log_parts1);
+
+        // Second Partition Pass:
+        // partition_pass_two: A CUDA kernel that refines the partitioning, redistributing keys and values into final buckets based on log_parts2.
+        partition_pass_two<NT, VT> <<<sm_counts, NT, p2_sm_bytes>>>(
+                                        keys_out,
+                                        vals_out,
+                                        chains[0],
+                                        buckets_used[1],
+                                        heads[1],
+                                        chains[1],
+                                        cnts[1],
+                                        keys,
+                                        vals,
+                                        log_parts2,
+                                        first_bit,
+                                        buckets_used[0],
+                                        log2_bucket_size);
+    }
+
+
+    void partition() {
+
+        partition(r_key_partitions, r_key_partitions_temp,
+                  (int*)(r_val_partitions), (int*)(r_val_partitions_temp),
+                  nr,
+                  buckets_num_max_R, heads_R, cnts_R, chains_R, buckets_used_R);
+
+        partition(s_key_partitions, s_key_partitions_temp,
+                  (int*)(s_val_partitions), (int*)(s_val_partitions_temp),
+                  ns,
+                  buckets_num_max_S, heads_S, cnts_S, chains_S, buckets_used_S);
+
+    }
+
+    void balance_buckets() {
+        decompose_chains <<<(1 << log_parts1), 1024>>> (bucket_info_R, chains_R[1], cnts_R[1], log_parts1 + log_parts2, 2*bucket_size, bucket_size);
+    }
+
+    void hash_join() {
+        constexpr int NT = 512;
+        constexpr int VT = 4;
 
 
     }
 
-    ~PartitionHashJoin() {}
+    void swap_r_s() {
+        // Swap the key partitions of R and S.
+        // Swap the value partitions of R and S.
+        //  Swap the match indices used in the join process.
+        std::swap(r_key_partitions, s_key_partitions);
+        std::swap(r_val_partitions, s_val_partitions);
+        std::swap(r_match_idx, s_match_idx);
+        /*
+        Iterates over index i for two elements:
+        chains_R[i] and chains_S[i]: Swap the chain metadata for R and S.
+        cnts_R[i] and cnts_S[i]: Swap the count metadata.
+        heads_R[i] and heads_S[i]: Swap the head pointers for bucket lists.
+        buckets_used_R[i] and buckets_used_S[i]: Swap the used bucket metadata.
+        */
+        for (int i = 0; i < 2; i++) {
+            std::swap(chains_R[i], chains_S[i]);
+            std::swap(cnts_R[i], cnts_S[i]);
+            std::swap(heads_R[i], heads_S[i]);
+            std::swap(buckets_used_R[i], buckets_used_S[i]);
+        }
+        /*
+        This function effectively reverses the roles of R and S, which can be beneficial for
+        ensuring that both datasets are evenly processed. It swaps all relevant data and metadata
+        structures to achieve this.
+        */
+    }
 
-private:
 
-    void partition() {
+
+    // no need to do this because it is done in cudf.
+    void materialize_by_gather(){
 
     }
 
