bf957f802267999e0edd144a142d6f436c9f502c
fejiang <fejiang@nvidia.comm>
2024-08-25 10:09:18 +0800

    phj.print_match_indices()

--- src/main/cpp/src/bucket_chain_hash_join.cu
+++ src/main/cpp/src/bucket_chain_hash_join.cu
@@ -37,6 +37,7 @@ inner_join(table_view const& left_input,
     //SortHashJoin shj(left_input, right_input, 15, 0, 5);
     PartitionHashJoin phj(left_input, right_input, 6, 9, 0, 1000);
     phj.join();
+    phj.print_match_indices();
     // shj.test_column_factories();
     return {std::move(left_vector), std::move(right_vector)};
 
