ede4fe02bd295b5a452a0bc8041c36c2223552ea
fejiang <fejiang@nvidia.com>
2024-10-24 15:53:43 +0800

    debug loggin removed
    Signed-off-by: fejiang <fejiang@nvidia.com>

--- src/test/java/com/nvidia/spark/rapids/jni/SortHashJoinTest.java
+++ src/test/java/com/nvidia/spark/rapids/jni/SortHashJoinTest.java
@@ -12,10 +12,12 @@ public class SortHashJoinTest {
         Table.TestBuilder tb1 = new Table.TestBuilder();
         tb1.column(1, 9, 2, 3, 4, 3,99999);
         tb1.column(1, 9, 2, 3, 4, 3, 9);
+        tb1.column(10, 90, 2, 3, 4, 3, 9);
 
         Table.TestBuilder tb2 = new Table.TestBuilder();
         tb2.column(1, 9, 2, 3, 6, 8, 4, 12, 14, 19, 99999);
         tb2.column(1, 9, 2, 3, 6, 8, 777, 12, 14, 19, 9);
+        tb2.column(10, 90, 2, 3, 6, 8, 777, 12, 14, 19, 9);
 
         Table left = tb1.build();
         Table right = tb2.build();
