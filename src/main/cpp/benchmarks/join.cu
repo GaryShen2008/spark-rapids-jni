2a3e776337448de4ebedc734a542133128ffd74e
fejiang <fejiang@nvidia.com>
2024-12-06 10:43:25 +0800

    some change on benchmark

--- src/main/cpp/benchmarks/join.cu
+++ src/main/cpp/benchmarks/join.cu
@@ -92,9 +92,9 @@ NVBENCH_BENCH_TYPES(nvbench_inner_join, NVBENCH_TYPE_AXES(JOIN_KEY_TYPE_RANGE, J
   .add_int64_axis("left_size", JOIN_SIZE_RANGE)
   .add_int64_axis("right_size", JOIN_SIZE_RANGE);
 
-// NVBENCH_BENCH_TYPES(nvbench_inner_join2, NVBENCH_TYPE_AXES(JOIN_KEY_TYPE_RANGE, JOIN_NULLABLE_RANGE))
-//   .set_name("inner_join2")
-//   .set_type_axes_names({"Key", "Nullable"})
-//   .add_int64_axis("left_size", JOIN_SIZE_RANGE)
-//   .add_int64_axis("right_size", JOIN_SIZE_RANGE);
+NVBENCH_BENCH_TYPES(nvbench_inner_join2, NVBENCH_TYPE_AXES(JOIN_KEY_TYPE_RANGE, JOIN_NULLABLE_RANGE))
+  .set_name("inner_join2")
+  .set_type_axes_names({"Key", "Nullable"})
+  .add_int64_axis("left_size", JOIN_SIZE_RANGE)
+  .add_int64_axis("right_size", JOIN_SIZE_RANGE);
 
