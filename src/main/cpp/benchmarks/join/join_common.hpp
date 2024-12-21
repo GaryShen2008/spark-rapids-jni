66b9696086cf62db4bb0db24926bbee2fce77d9c
fejiang <fejiang@nvidia.comm>
2024-12-21 21:45:29 +0800

    comment logging

--- src/main/cpp/benchmarks/join/join_common.hpp
+++ src/main/cpp/benchmarks/join/join_common.hpp
@@ -49,7 +49,7 @@ using JOIN_KEY_TYPE_RANGE = nvbench::type_list<nvbench::int32_t>;
 using JOIN_NULLABLE_RANGE = nvbench::enum_type_list<false>;
 using tracking_adaptor = rmm::mr::tracking_resource_adaptor<rmm::mr::device_memory_resource>;
 
-auto const JOIN_SIZE_RANGE = std::vector<nvbench::int64_t>{1'000'000};
+auto const JOIN_SIZE_RANGE = std::vector<nvbench::int64_t>{10'000'000};
 
 struct null75_generator {
   thrust::minstd_rand engine;
